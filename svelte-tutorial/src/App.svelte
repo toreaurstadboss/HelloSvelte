7cbeee21ae3c2789aa6b77ee23378b9fd60c33d2
Tore Aurstad <tore.aurstad@hemit.no>
2019-10-13 23:54:10 +0200

    Svelte

---
+++ svelte-tutorial/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  let user = { loggedIn: false };
+
+  function toggle() {
+    user.loggedIn = !user.loggedIn;
+    console.log("weeee");
+    var mark = document.createElement("marquee");
+    mark.innerHTML = "Svelte";
+    document.body.appendChild(mark);
+  }
+</script>
+
+{#if user.loggedIn}
+  <button on:click={toggle}>Log out</button>
+{/if}
+
+{#if !user.loggedIn}
+  <button on:click={toggle}>Log in</button>
+{/if}
